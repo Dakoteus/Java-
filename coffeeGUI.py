f35faa3a4ea8d23aa98ad1f0aadc3d08734db501
Dakoteus <dakoteus2117@gmail.com>
2015-06-05 12:59:38 -0400

    because

---
+++ coffeeGUI.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+class Application(Frame):
+    def brewit(self):
+        print("Running code...")
+
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"]   = "red"
+        self.QUIT["command"] =  self.quit
+
+        self.QUIT.pack({"side": "left"})
+
+        self.button1 = Button(self)
+        self.button1["text"] = "Brew",
+        self.button1["fg"] = "brown"
+        self.button1["command"] = self.brewit
+
+        self.button1.pack({"side": "left"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
